4c8a284bef6f40a9193639f7662bf38a9c2690b4
Qianqian Tong <qit16101@gmail.com>
2018-06-21 14:24:27 -0400

    sample 1 for first lecture

--- Lecture1/sample1/src/hello.cpp
+++ Lecture1/sample1/src/hello.cpp
@@ -5,10 +5,18 @@
 #include "vector.h"  // for Vector
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    int age = getInteger("How old are you? ");
-    cout << "Yeah right ! Qianqian wishes she were " << age << "!";
+void song() {                                   // write a function without variable
+    cout << "Now this is the story" << endl;    // endl=/n
+    cout << "all about how" << endl;
+    cout << "My life got flipped" << endl;
+    cout << "turned upside down." << endl;
+}
 
+int main() {
+    song();                                     // call song function
+    cout << "Hello, World!" << endl;
+    int age = getInteger("How old are you? ");  // call getInteger from simpio.cpp (follow symbol under cursor)
+    cout << "Yeah right ! Qianqian wishes she were " << age << "!" << endl;
+    song();
     return 0;
 }
