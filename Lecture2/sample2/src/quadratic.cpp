8375359b088575a90bf667fa55251ce6bfa4a693
Qianqian Tong <qit16101@gmail.com>
2018-06-22 14:26:17 -0400

    quadratic

---
+++ Lecture2/sample2/src/quadratic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include "console.h"
+#include "strlib.h"
+#include "gmath.h"
+using namespace std;
+
+void quadratic(int a, int b, int c, double& root1, double& root2){
+    double d = sqrt(b*b-4*a*c);
+    if(d>=0){
+        root1 = (-b+d)/(2*a);
+        root2 = (-b-d)/(2*a);
+    }
+}
+
+int main() {
+    double root1,root2;
+    quadratic(1,-3,-4,root1,root2);
+    cout << "root1:"<< root1<<" root2:" << root2 << endl;
+    return 0;
+}
